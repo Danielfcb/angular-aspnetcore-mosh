38d09bddda17f2dba4e73ffc4f7772a5b02b385f
Daniel Rooein <Daniel.Rooein@gmail.com>
2019-04-14 22:26:37 -0400

    Updated the seeding queries

---
+++ vega/Migrations/20190415021032_SeeDatabase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class SeeDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('honda')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('toyota')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('mazda')");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('civic',1)");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('accord',1)");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('corolla',2)");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('camry',2)");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('model3',3)");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('model6',3)");
+
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Makes where name in ('honda','toyota','mazda')");
+        }
+    }
+}
